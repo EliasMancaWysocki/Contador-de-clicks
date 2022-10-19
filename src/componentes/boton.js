7e45bce4f0aaeb0805fc787a8b13241fb896ee05
Hyperion04 <eliasmanca20@gmail.com>
2022-10-19 18:00:51 -0300

    Creado el repositorio y subida la app finalizada

---
+++ src/componentes/boton.js
@@ -0,0 +1,11 @@
+import '../stylesheets/Boton.css'
+
+export function Boton({ texto, esBotonDeClick, manejarClick }) {
+  return (
+    <button 
+    className={ esBotonDeClick ? 'boton-click' : 'boton-reiniciar' }
+    onClick={manejarClick} >
+      {texto}
+    </button>
+  )
+}
